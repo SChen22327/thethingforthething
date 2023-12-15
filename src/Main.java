6bcd00d2e0e564665e86df5ccc87a565a26a8c08
BT_1N3_15 <sample@bths.edu>
2023-12-15 08:58:09 -0500

    changed first print statement in main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("changing this string to something");
         System.out.println("Hello this new concept is very interesting");
         System.out.println("Hello");
     }
