d665e9e724e60194c6321e6856e277a4dc9b575a
BT_1N3_15 <sample@bths.edu>
2023-12-15 09:06:37 -0500

    changed third print

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("changing this string to something");
         System.out.println("Hello this new concept is very interesting");
-        System.out.println("Hello");
+        System.out.println("bye bye");
     }
 }
